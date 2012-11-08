1f8a0774d98e09a664d5038c7d9b21c602822fbe
Malena Andrade <malena.andrade@gmail.com>
2012-11-08 17:28:34 -0500

    Added GSAP TweenMax JS platform.

--- calculator/js/calculator.interface.js
+++ calculator/js/calculator.interface.js
@@ -1,4 +1,12 @@
 $(function() {
 
+    $('.numbers li').on('click', function(){
+        var button = $(this);
+        TweenLite.to(button, 1, {css:{
+            borderTop: 'none',
+            borderBottom: 'solid 1px #eee',
+            boxShadow: 'inset 0 1px 4px rgba(0, 0, 0, 0.6)'}
+        });
+    });
 
 });
