4effbfd570b31d969e8fd0a3b46e30976898cd9f
Malena Andrade <malena.andrade@gmail.com>
2012-11-13 15:25:14 -0500

    Moved button animation js to interface.js. Kept event logic interface.logic.js

--- calculator/js/calculator.interface.logic.js
+++ calculator/js/calculator.interface.logic.js
@@ -1,9 +1,34 @@
 $(function() {
 
-    /* On click store number */
+    /* Number Set Array */
+    var numberSet = [ 0 ];
+    var operators = [null];
+
     $('.numbers li').on('click', function(){
+
         var digit = $(this).attr('id');
-        console.log(digit);
+
+
+        /* Display Digit */
+        if(!isNaN(digit)){
+            $('.digit').append(digit);
+        }
+
+
+        /*  If operator clicked
+            get current number displayed
+            store current number displayed
+            store current operator
+        */
+
+        if($(this).hasClass('operator')){
+            operators[1] = $(this).attr('id');
+            numberSet[1] = $('.digit').text();
+            console.log(numberSet);
+            console.log(operators);
+            $('.digit').text('');
+        }
+
     });
 
 
