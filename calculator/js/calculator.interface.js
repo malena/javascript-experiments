0d13673db1207dec6d098045a578dca1a2c09bea
Malena Andrade <malena.andrade@gmail.com>
2012-11-13 15:27:02 -0500

    Cleaned up js

--- calculator/js/calculator.interface.js
+++ calculator/js/calculator.interface.js
@@ -1,13 +1,8 @@
 $(function() {
 
-    /* Number Set Array */
-    var numberSet = [ 0 ];
-    var operators = [null];
-
     $('.numbers li').on('click', function(){
 
         var button = $(this);
-        var digit = $(this).attr('id');
         var buttonClicked = new TimelineLite();
 
         /* Button Click */
@@ -17,28 +12,6 @@ $(function() {
             boxShadow: 'inset 0 1px 4px rgba(0, 0, 0, 0.6)'},
             onComplete: buttonClicked.reverse()
         });
-
-        /* Display Digit */
-        if(!isNaN(digit)){
-            $('.digit').append(digit);
-        }
-
-
-        /*  If operator clicked
-            get current number displayed
-            store current number displayed
-            store current operator
-        */
-
-        if($(this).hasClass('operator')){
-            operators[1] = $(this).attr('id');
-            numberSet[1] = $('.digit').text();
-            console.log(numberSet);
-            console.log(operators);
-            $('.digit').text('');
-        }
-
     });
-
 
 });
--- calculator/js/calculator.interface.logic.js
+++ calculator/js/calculator.interface.logic.js
@@ -1,35 +1,3 @@
-$(function() {
-
-    /* Number Set Array */
-    var numberSet = [ 0 ];
-    var operators = [null];
-
-    $('.numbers li').on('click', function(){
-
-        var digit = $(this).attr('id');
-
-
-        /* Display Digit */
-        if(!isNaN(digit)){
-            $('.digit').append(digit);
-        }
-
-
-        /*  If operator clicked
-            get current number displayed
-            store current number displayed
-            store current operator
-        */
-
-        if($(this).hasClass('operator')){
-            operators[1] = $(this).attr('id');
-            numberSet[1] = $('.digit').text();
-            console.log(numberSet);
-            console.log(operators);
-            $('.digit').text('');
-        }
-
-    });
-
-
-});
+Unstaged changes after reset:
+M	calculator/js/calculator.interface.js
+M	calculator/js/calculator.interface.logic.js
