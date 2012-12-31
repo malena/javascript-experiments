e547934e2721b4e795819d049de3ec50ca95ef7e
Malena Andrade <malena.andrade@gmail.com>
2012-12-31 10:59:02 -0500

    Calculator operations and correct result display

--- calculator/js/calculator.js
+++ calculator/js/calculator.js
@@ -1,34 +1,131 @@
-function Calculator(){}
+function Calculator(){
+    this.currentValue = null;
+    this.lastOperator = null;
+    var stringValue;
+}
+
 
 Calculator.prototype.inputDigit = function(digit){
-
     if(this.currentValue === null){
         this.currentValue = digit;
-    } else {
-        var stringValue = '' + this.currentValue + digit;
-        this.currentValue = parseInt(stringValue, 10);
+        return this.currentValue;
     }
-
-    return this.currentValue;
+    if(this.lastOperator !== null){
+        $('.display').text('');
+        stringValue = digit;
+        this.currentValue = stringValue;
+        return this.currentValue;
+    } else {
+        stringValue = '' + this.currentValue + digit;
+        this.currentValue = stringValue;
+        return this.currentValue;
+    }
 };
 
 Calculator.prototype.add = function(){
-
     if(this.currentValue === null){
         return 0;
     }
     if(this.lastOperator === null){
         this.lastOperator = 'add';
-        this.lastResutls = this.currentValue;
-    }
-    else {
+        this.lastResult = this.currentValue;
+        return this.lastResult;
+    } else {
         switch(this.lastOperator){
             case 'add':
-            this.lastResult = this.lastResult + this.currentValue;
+            this.lastResult = parseInt(this.lastResult,10) + parseInt(this.currentValue,10);
             break;
             case 'subtract':
-            this.lastResult = this.lastResult - this.currentValue;
+            this.lastResult = parseInt(this.lastResult,10) - parseInt(this.currentValue,10);
+            break;
+            case 'multiply':
+            this.lastResult = parseInt(this.lastResult,10) * parseInt(this.currentValue,10);
+            break;
+            case 'divide':
+            this.lastResult = parseInt(this.lastResult,10) / parseInt(this.currentValue,10);
             break;
         }
+        return this.lastResult;
+    }
+};
+
+Calculator.prototype.subtract = function(){
+    if(this.currentValue === null){
+        return 0;
+    }
+    if(this.lastOperator === null){
+        this.lastOperator = 'subtract';
+        this.lastResult = this.currentValue;
+        return this.lastResult;
+    } else {
+        switch(this.lastOperator){
+            case 'add':
+            this.lastResult = parseInt(this.lastResult,10) + parseInt(this.currentValue,10);
+            break;
+            case 'subtract':
+            this.lastResult = parseInt(this.lastResult,10) - parseInt(this.currentValue,10);
+            break;
+            case 'multiply':
+            this.lastResult = parseInt(this.lastResult,10) * parseInt(this.currentValue,10);
+            break;
+            case 'divide':
+            this.lastResult = parseInt(this.lastResult,10) / parseInt(this.currentValue,10);
+            break;
+        }
+        return this.lastResult;
+    }
+};
+
+Calculator.prototype.multiply = function(){
+    if(this.currentValue === null){
+        return 0;
+    }
+    if(this.lastOperator === null){
+        this.lastOperator = 'multiply';
+        this.lastResult = this.currentValue;
+        return this.lastResult;
+    } else {
+        switch(this.lastOperator){
+            case 'add':
+            this.lastResult = parseInt(this.lastResult,10) + parseInt(this.currentValue,10);
+            break;
+            case 'subtract':
+            this.lastResult = parseInt(this.lastResult,10) - parseInt(this.currentValue,10);
+            break;
+            case 'multiply':
+            this.lastResult = parseInt(this.lastResult,10) * parseInt(this.currentValue,10);
+            break;
+            case 'divide':
+            this.lastResult = parseInt(this.lastResult,10) / parseInt(this.currentValue,10);
+            break;
+        }
+        return this.lastResult;
+    }
+};
+
+Calculator.prototype.divide = function(){
+    if(this.currentValue === null){
+        return 0;
+    }
+    if(this.lastOperator === null){
+        this.lastOperator = 'divide';
+        this.lastResult = this.currentValue;
+        return this.lastResult;
+    } else {
+        switch(this.lastOperator){
+            case 'add':
+            this.lastResult = parseInt(this.lastResult,10) + parseInt(this.currentValue,10);
+            break;
+            case 'subtract':
+            this.lastResult = parseInt(this.lastResult,10) - parseInt(this.currentValue,10);
+            break;
+            case 'multiply':
+            this.lastResult = parseInt(this.lastResult,10) * parseInt(this.currentValue,10);
+            break;
+            case 'divide':
+            this.lastResult = parseInt(this.lastResult,10) / parseInt(this.currentValue,10);
+            break;
+        }
+        return this.lastResult;
     }
 };
--- calculator/js/calculatorView.js
+++ calculator/js/calculatorView.js
@@ -7,19 +7,35 @@ function CalculatorView(){
     $('.digit').each(function(){
         $(this).click(function(){
             var $digit = $(this).text();
-            var currentValue = calculator.inputDigit($digit);
+            var currentValue = $digit;
+            currentValue = calculator.inputDigit($digit);
             $display.text(currentValue);
         });
     });
 
     $('#add').click(function(){
         var result = calculator.add();
-        $display.val(result);
+        $display.text(result);
     });
 
     $('#equals').click(function(){
         var result = calculator.equals();
-        $display.val(result);
+        $display.text(result);
+    });
+
+    $('#subtract').click(function(){
+        var result = calculator.subtract();
+        $display.text(result);
+    });
+
+    $('#multiply').click(function(){
+        var result = calculator.multiply();
+        $display.text(result);
+    });
+
+    $('#divide').click(function(){
+        var result = calculator.divide();
+        $display.text(result);
     });
 }
 
