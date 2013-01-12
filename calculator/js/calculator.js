f73e2ec6dcd1921f28ed44d8e1cce9d02a313c01
Malena Andrade <malena.andrade@gmail.com>
2013-01-12 14:30:15 -0500

    Removed clearState null value and if statement

--- calculator/js/calculator.js
+++ calculator/js/calculator.js
@@ -3,28 +3,28 @@ function Calculator(){
     this.lastOperator = null;
     this.currentOperator = null;
     this.stringValue = '';
-    this.clearState = null;
+    this.clearState = false;
 }
 
 Calculator.prototype.inputDigit = function(digit){
     if(this.currentValue === null){
         this.currentValue = digit;
+        console.log('I\'m null!');
         return this.currentValue;
     }
-    if(this.clearState !== null){
-
+    else {
         if(this.clearState === true) {
             this.currentValue = digit;
             console.log('display cleared');
             this.clearState = false;
             return this.currentValue;
-        } else if (this.clearState === false){
+        }
+        else {
             this.stringValue = '' + this.currentValue + digit;
             this.currentValue = this.stringValue;
             return this.currentValue;
         }
 
-    } else {
         this.stringValue = '' + this.currentValue + digit;
         this.currentValue = this.stringValue;
         return this.currentValue;
