1cd71630c2efec54afd97cb56c136c8052831466
Malena Andrade <mandrade@mandrade.local>
2013-02-12 12:05:37 -0500

    Hangman logic outline

--- hangman/js/hangmanLogic.js
+++ hangman/js/hangmanLogic.js
@@ -1,3 +1,27 @@
-function Hangman(){
+$(document).ready(function(){
 
-}
+	function Hangman(){
+	}
+
+	Hangman.prototype.isLetterMatch = function(){
+
+	}
+
+	Hangman.prototype.flipCard = function(){
+
+	}
+
+	Hangman.prototype.checkIfAllCardsFlipped = function(){
+
+	}
+
+	Hangman.prototype.wordComplete = function(){
+		
+	}
+
+	Hangman.prototype.dumpLetter = function(){
+		
+	}
+
+
+});
