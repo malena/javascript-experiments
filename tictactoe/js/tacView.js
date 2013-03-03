209e85450f3c12cc9c66cd95e29270ffb091e241
Malena Andrade <malena.andrade@gmail.com>
2013-03-03 15:15:01 -0500

    Renaming scripts, default css

--- tictactoe/js/tacView.js
+++ tictactoe/js/tacView.js
@@ -1,48 +1,10 @@
-function HangmanView(){
-    var animate = new HangmanAnimations({});
+function TicTacToeView(){
+
+    var animate = new TicTacToeAnimations({});
     animate.initialize();
-	var hangman = new HangmanWordDisplay({});
-	hangman.initialize();
-    
-	var hangman_word_array = hangman.word_array;
-    var hangman_word_letter_count = hangman.letter_count;
-    var incorrect = 1;
-    var index_correct = 0;
+	var tictactoe = new TicTacToe({});
+	tictactoe.initialize();
 
-    $('.letter-guess').submit(function(e){
-        e.preventDefault();
-
-        var letter = $(this).find('input').val(); // input letter
-        var letter_index = hangman_word_array.indexOf(letter); // letter index
-
-        // dump letter
-    	if(letter_index == -1){
-            //keep track of number of incorrect letters
-    		//animate incorrect letter
-            animate.showBodyPart(incorrect);
-            if (incorrect == 6) {
-                setTimeout(function(){
-                    animate.endGame('lose');
-                }, 1000);
-            }
-            animate.dumpLetter(letter);
-            incorrect++
-    	}
-        //if there is a return value then flip letter of matching index
-        else {
-            index_correct++;
-            if(index_correct == hangman.letter_count){
-                $("div.guess p:contains('" + letter + "')").parent().parent().addClass('flip');
-                setTimeout(function(){
-                    $.proxy(animate.endGame('win'), animate);
-                }, 1000);
-            } else {
-                $("div.guess p:contains('" + letter + "')").parent().parent().addClass('flip');
-            }
-        } 
-
-        //reset input box
-        $('form input').focus().val('');
-    });
+    //This contains game events
 
 }
