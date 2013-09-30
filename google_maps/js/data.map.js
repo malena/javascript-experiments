9d23ad8f72677cc7bf7ada58cc9cc39ce3f8a30c
Malena Andrade <malena.andrade@gmail.com>
2013-09-29 21:25:43 -0400

    Refactored, separated map marker init  Please enter the commit message for your changes. Lines starting

--- google_maps/js/data.map.js
+++ google_maps/js/data.map.js
@@ -1,27 +1,58 @@
 function MapData(options){
 
-    // merge default object properties 
-    // with what was passsed in index.html
-    // used if you had default properties to set in this object
-    // that you use for methods in this object scope.
-    // for now literally pass data via index.html page
-
     var defaults = {};
 
     this.config = $.extend(true, defaults, options || { });
-
-    this.geocoder = new google.maps.Geocoder();
 
     this.map_options = {
     	zoom: 1,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
 
+    this.locationsArray = this.generateLocationsArray(); 
 	this.initialize();
 };
 
 MapData.prototype.initialize = function(){
 	this.initMap();
+};
+
+
+
+MapData.prototype.initMap = function() {
+	this.centerMap();
+    var map = new google.maps.Map(document.getElementById("map-canvas"), this.map_options);
+    this.initMarkers(map);
+};
+
+MapData.prototype.initMarkers = function(map){
+	var self = this;
+    var marker, i;
+
+    for(i = 0; i < this.locationsArray.length; i++){
+	    marker = new google.maps.Marker({
+	        map : map,
+	        draggable: true,
+	        animation: google.maps.Animation.DROP,
+	        position: new google.maps.LatLng(self.locationsArray[i][0] , self.locationsArray[i][1])
+	    });
+    }
+
+    this.bindInfoWindow(this.initInfoWindow(), map, marker);
+};
+
+MapData.prototype.initInfoWindow = function(){
+    var infowindow = new google.maps.InfoWindow({
+    	content: this.getInfoWindowContent(),
+    	maxWidth: 200
+    });
+    return infowindow;
+};
+
+MapData.prototype.bindInfoWindow = function(info_window, map, marker){
+    google.maps.event.addListener(marker, 'mouseover', function() {
+	    info_window.open(map,marker);
+	});
 };
 
 MapData.prototype.generateLocationsArray = function() {
@@ -35,53 +66,14 @@ MapData.prototype.generateLocationsArray = function() {
 		lng = this.config.data[i].longitude;
 		locationsArray.push([lat, lng]);
 	}
-
 	return locationsArray;
 };
 
-MapData.prototype.initMap = function() {
-	var array = this.generateLocationsArray();
 
-	var default_lat = array[0][0];
-	var default_lng = array[0][1];
-
-    this.map_options['center'] = new google.maps.LatLng(default_lat, default_lng);
-
-    var map = new google.maps.Map(document.getElementById("map-canvas"), this.map_options);
-
-    var coord;
-
-    var marker, i;
-
-    for(i = 0; i < array.length; i++){
-	    marker = new google.maps.Marker({
-	        map : map,
-	        draggable: true,
-	        animation: google.maps.Animation.DROP,
-	        position: new google.maps.LatLng(array[i][0] , array[i][1])
-	    });
-    }
-
-    var info_window = this.initInfoWindow();
-
-    this.bindMarker(info_window, map, marker);
-
-};
-
-MapData.prototype.initInfoWindow = function(){
-
-    var infowindow = new google.maps.InfoWindow({
-    	content: this.getInfoWindowContent(),
-    	maxWidth: 200
-    });
-
-    return infowindow;
-};
-
-MapData.prototype.bindMarker = function(info_window, map, marker){
-    google.maps.event.addListener(marker, 'mouseover', function() {
-	    info_window.open(map,marker);
-	});
+MapData.prototype.centerMap = function(){
+	var lat = this.locationsArray[0][0];
+	var lng = this.locationsArray[0][1];
+    this.map_options['center'] = new google.maps.LatLng(lat, lng);
 };
 
 MapData.prototype.getInfoWindowContent = function() {
